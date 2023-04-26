667150eb4c32bf068c65af17ab431e2af148a54d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-04-26 15:00:06 +0800

    :sparkles: 获取IP地址

--- xxwcli.go
+++ xxwcli.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"github.com/urfave/cli"
+	"fmt"
+	"net"
 	"os"
+
+	"github.com/urfave/cli"
 )
 
 var (
@@ -10,7 +13,7 @@ var (
 	Version     = "v0.0.1"
 	Author      = "xuxiaowei-com-cn/xxwcli: https://github.com/xuxiaowei-com-cn/xxwcli"
 	Email       = "徐晓伟 <xuxiaowei@xuxiaowei.com.cn>"
-	Copyright   = "徐晓伟"
+	Copyright   = "徐晓伟 <xuxiaowei@xuxiaowei.com.cn>"
 	Description = "命令行工具"
 )
 
@@ -21,12 +24,72 @@ func main() {
 	app.Version = Version
 	app.Author = Author
 	app.Email = Email
+	app.Usage = Description
 	app.Copyright = Copyright
-	app.Description = Description
 
-	err := (app).Run(os.Args)
-	if err != nil {
-		return
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "获取当前计算机的 IP 地址",
+			Action: func(c *cli.Context) error {
+				addrs, err := net.InterfaceAddrs()
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				for _, addr := range addrs {
+					if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+						if ipnet.IP.To4() != nil {
+							fmt.Println(ipnet.IP.String())
+						} else if ipnet.IP.To16() != nil {
+							fmt.Println(ipnet.IP.String())
+						}
+					}
+				}
+				return nil
+			},
+		},
+
+		{
+			Name:  "ipv4",
+			Usage: "获取当前计算机的 IPv4 地址",
+			Action: func(c *cli.Context) error {
+				addrs, err := net.InterfaceAddrs()
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				for _, addr := range addrs {
+					if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+						fmt.Println(ipnet.IP.String())
+					}
+				}
+				return nil
+			},
+		},
+
+		{
+			Name:  "ipv6",
+			Usage: "获取当前计算机的 IPv6 地址",
+			Action: func(c *cli.Context) error {
+				addrs, err := net.InterfaceAddrs()
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				for _, addr := range addrs {
+					if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To16() != nil && ipnet.IP.To4() == nil {
+						fmt.Println(ipnet.IP.String())
+					}
+				}
+				return nil
+			},
+		},
 	}
 
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
