5124a8bd475bc0899ff5632ebb4ce53030a15784
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-06-05 08:28:57 +0800

    :sparkles: 生成 RSA 秘钥文件

---
+++ rsa/rsa.go
@@ -0,0 +1,78 @@
+package rsa
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+// GenerateRSAKeyPair 生成 RSA 秘钥文件
+func GenerateRSAKeyPair(keyLength int, privateKeyPath string, publicKeyPath string) error {
+	// 生成密钥对，大小为 keyLength 比特
+	privateKey, err := rsa.GenerateKey(rand.Reader, keyLength)
+	if err != nil {
+		return err
+	}
+
+	// 把私钥编码成 PEM 格式字符串并输出到文件中
+	privateKeyFile, err := os.Create(privateKeyPath)
+	if err != nil {
+		return err
+	}
+	defer func(privateKeyFile *os.File) {
+		err := privateKeyFile.Close()
+		if err != nil {
+
+		}
+	}(privateKeyFile)
+	privateKeyPEM := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+	err = pem.Encode(privateKeyFile, privateKeyPEM)
+	if err != nil {
+		return err
+	}
+
+	// 把公钥编码成 PEM 格式字符串并输出到文件中
+	publicKey := &privateKey.PublicKey
+	publicKeyFile, err := os.Create(publicKeyPath)
+	if err != nil {
+		return err
+	}
+	defer func(publicKeyFile *os.File) {
+		err := publicKeyFile.Close()
+		if err != nil {
+
+		}
+	}(publicKeyFile)
+	publicKeyPEM, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return err
+	}
+	publicKeyPEMBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: publicKeyPEM,
+	}
+	err = pem.Encode(publicKeyFile, publicKeyPEMBlock)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("RSA 密钥对已生成成功！")
+	return nil
+}
+
+//func main() {
+//	keyLength := 2048                   // 设置默认的秘钥长度
+//	privateKeyPath := "private_key.pem" // 设置默认的私钥文件保存路径
+//	publicKeyPath := "public_key.pem"   // 设置默认的公钥文件保存路径
+//
+//	err := GenerateRSAKeyPair(keyLength, privateKeyPath, publicKeyPath)
+//	if err != nil {
+//		panic(err)
+//	}
+//}
